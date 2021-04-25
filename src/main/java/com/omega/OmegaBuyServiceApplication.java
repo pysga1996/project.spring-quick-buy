ab7102706c87d72bc89b385cdea102d040012355
thanhvt <thanhvt@vissoft.vn>
2021-04-25 23:15:57 +0700

    update jwk

--- src/main/java/com/omega/OmegaBuyServiceApplication.java
+++ src/main/java/com/omega/OmegaBuyServiceApplication.java
@@ -1,6 +1,5 @@
 package com.omega;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
