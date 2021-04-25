85230a891e0674d1b1a00f1404238c8015a3bb67
thanhvt <thanhvt@vissoft.vn>
2021-04-26 01:37:55 +0700

    update

--- src/main/java/com/omega/OmegaBuyServiceApplication.java
+++ src/main/java/com/omega/OmegaBuyServiceApplication.java
@@ -2,11 +2,13 @@ package com.omega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = "com.omega")
+@SpringBootApplication(scanBasePackages = "com.omega", exclude = {
+    OAuth2ResourceServerAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = "com.omega.repositories")
 public class OmegaBuyServiceApplication {
 
