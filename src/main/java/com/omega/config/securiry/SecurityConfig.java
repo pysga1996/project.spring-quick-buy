1f62ea01a9b31d9ff4188945d43cbf103535cea6
thanhvt <thanhvt@vissoft.vn>
2021-04-26 01:14:58 +0700

    update

--- src/main/java/com/omega/config/securiry/SecurityConfig.java
+++ src/main/java/com/omega/config/securiry/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 
