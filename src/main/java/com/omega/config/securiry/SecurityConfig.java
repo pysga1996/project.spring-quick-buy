d0e902e530e05e0788fac0c9e25a3043c9c8f7c5
thanhvt <thanhvt@vissoft.vn>
2021-04-26 01:43:10 +0700

    update

--- src/main/java/com/omega/config/securiry/SecurityConfig.java
+++ src/main/java/com/omega/config/securiry/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.omega.config.securiry;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.cloud.CloudPlatform;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -9,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
-import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 
 @Configuration
@@ -19,11 +19,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 //    private static final Logger log = LogManager.getLogger(SecurityConfig.class);
 
-    private final OpaqueTokenIntrospector opaqueTokenIntrospector;
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+//    private OpaqueTokenIntrospector opaqueTokenIntrospector;
+
+    private final Environment env;
 
     @Autowired
-    public SecurityConfig(OpaqueTokenIntrospector opaqueTokenIntrospector) {
-        this.opaqueTokenIntrospector = opaqueTokenIntrospector;
+    public SecurityConfig(Environment env) {
+        this.env = env;
     }
 
 //    @Autowired(required = false)
@@ -41,25 +46,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .requiresChannel()
-                // Heroku https config
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure().and()
-                .authorizeRequests()
-                .antMatchers("/api/admin/**").access("hasRole('ADMIN')")
-                .antMatchers("/oauth/token", "/api/login", "/api/register", "/api/song/download/**", "/api/song/upload", "/api/album/upload", "/api/album/download/**", "/api/**").permitAll()
-                .and()
-                .csrf().disable()
-                .cors()
-                .and()
-                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
-                    DefaultBearerTokenResolver bearerTokenResolver = new DefaultBearerTokenResolver();
-                    bearerTokenResolver.setAllowUriQueryParameter(true);
-                    httpSecurityOAuth2ResourceServerConfigurer.opaqueToken(opaqueTokenConfigurer ->
-                            opaqueTokenConfigurer.introspector(opaqueTokenIntrospector)).bearerTokenResolver(bearerTokenResolver);
-                })
-                .headers()
-                .frameOptions().sameOrigin().disable()
-                .authorizeRequests().anyRequest().permitAll().and();
+            .requiresChannel()
+            // Heroku https config
+            .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure().and()
+            .authorizeRequests()
+            .antMatchers("/api/admin/**").access("hasRole('ADMIN')")
+            .antMatchers("/oauth/token", "/api/login", "/api/register", "/api/song/download/**",
+                "/api/song/upload", "/api/album/upload", "/api/album/download/**", "/api/**")
+            .permitAll()
+            .and()
+            .csrf().disable()
+            .cors()
+            .and()
+            .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                DefaultBearerTokenResolver bearerTokenResolver = new DefaultBearerTokenResolver();
+                bearerTokenResolver.setAllowUriQueryParameter(true);
+                if (CloudPlatform.HEROKU.isActive(this.env)) {
+//                    log.info();
+                    System.out.println("Heroku config JWT");
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(jwtConfigurer ->
+                        jwtConfigurer.jwkSetUri(this.jwkSetUri));
+                }
+//                else {
+////                    log.info();
+//                    System.out.println("Non-Heroku config Opaque Token");
+//                    httpSecurityOAuth2ResourceServerConfigurer.opaqueToken(opaqueTokenConfigurer ->
+//                        opaqueTokenConfigurer.introspector(opaqueTokenIntrospector));
+//                }
+                httpSecurityOAuth2ResourceServerConfigurer.bearerTokenResolver(bearerTokenResolver);
+            })
+            .headers()
+            .frameOptions().sameOrigin().disable()
+            .authorizeRequests().anyRequest().permitAll().and();
     }
 }
